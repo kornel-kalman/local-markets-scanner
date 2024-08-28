664e62eb3ab43915aebd4dc6f00b8e49a1d424f2
Klorti <klortify@gmail.com>
2024-08-28 17:04:28 +0200

    Add pagination (in progress)

--- src/components/PlacesOverview.vue
+++ src/components/PlacesOverview.vue
@@ -1,21 +1,29 @@
 <script setup>
 import places_json from '../assets/places.json'
 
+/*** Table data ***/
 const places = places_json
 // Use index as sequence number
 for (let idx in places) {
   places[idx]['seq_no'] = idx
 }
+
+/*** Table properties ***/
 const fields = [
   'seq_no',
   {key: 'name', sortable: true},
   {key: 'location', sortable: true, label: '[Latitude, Longitude]'},
   {key: 'rating', sortable: true}
 ]
-const sortBy = 'rating'
-const sortDesc = true
-const map_link_template = 'https://maps.google.com/maps?q=LAT,LON&z=14'
 
+// const sortBy = 'rating'
+// const sortDesc = true
+// TODO Pagination does not affect table
+const perPage = 20
+const currentPage = 1
+
+
+const map_link_template = 'https://maps.google.com/maps?q=LAT,LON&z=14'
 
 const get_link = (location) => {
   return map_link_template
@@ -26,12 +34,19 @@ const get_link = (location) => {
 
 <template>
   <div class="content">
+    <b-pagination
+        v-model="currentPage"
+        :total-rows="places.length"
+        :per-page="perPage"
+        aria-controls="places-table"
+    />
     <b-table id="places-table"
              outlined hover caption-top
              caption="Local markets in Sri Lanka"
              :striped=true
              :small=true
-             per-page=20
+             :per-page=perPage
+             :current-page=currentPage
              :fields=fields
              :items=places
              :sort-by=sortBy
@@ -40,7 +55,7 @@ const get_link = (location) => {
     >
       <template #cell(location)="data">
         <a :href="get_link(data.item.location)" target="_blank" title="Open in Google Maps">
-          {{ data.item.location }}
+          ({{ data.item.location[0].toFixed(4) }}, {{ data.item.location[1].toFixed(4) }})
         </a>
       </template>
     </b-table>
