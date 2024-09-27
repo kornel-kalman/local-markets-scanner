04bb8493684549c5eee811cd87099ac300e6aabe
Klorti <klortify@gmail.com>
2024-09-27 18:16:10 +0200

    Add is_market flag full-stack.

---
+++ src/components/elements/VariantButton.vue
@@ -0,0 +1,34 @@
+<script setup>
+import {defineEmits, defineProps} from "vue";
+
+/*const props = */
+defineProps({
+  active: {
+    type: Boolean,
+    required: false
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  variantClass: {
+    type: String,
+    required: true
+  }
+})
+
+defineEmits(['clicked'])
+</script>
+
+<template>
+  <b-button :variant="(active ? '' : 'outline-') + variantClass"
+            @click="$emit('clicked')">
+    {{ text }}
+  </b-button>
+</template>
+
+<style scoped>
+button {
+  margin: 5px;
+}
+</style>
