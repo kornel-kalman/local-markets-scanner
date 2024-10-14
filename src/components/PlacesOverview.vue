e4a5b946e41777ab9ee840b505e732eb36fbe2df
Klorti <klortify@gmail.com>
2024-10-14 12:10:39 +0200

    Add multiple filtering options to table - by name & type

--- src/components/PlacesOverview.vue
+++ src/components/PlacesOverview.vue
@@ -1,13 +1,38 @@
 <script setup>
 import PlaceDetails from "@/components/PlaceDetails.vue";
 import axios from "axios";
-import {onBeforeMount, ref} from "vue";
+import {computed, onBeforeMount, ref} from "vue";
 
 
 /*** Table data ***/
 const places = ref([]);
 const displayedPlaces = ref([]);
 const dataAvailable = ref(false);
+
+/*** Table properties ***/
+const fields = [
+  {key: 'id', sortable: true},
+  {key: 'n_photos', sortable: true},
+  {key: 'n_reviews', sortable: true},
+  {key: 'name', sortable: true},
+  {key: 'rating', sortable: true},
+  {key: 'is_market', sortable: true}
+];
+
+const perPage = 20;
+const currentPage = ref(1);
+const totalRows = ref(0)
+
+const types = ref([])
+const typeFilter = ref(null)
+const textFilter = ref(null);
+
+const filter = computed(() => {
+  if (textFilter.value === null && typeFilter.value === null) {
+    return null;
+  }
+  return {'text': textFilter.value, 'type': typeFilter.value}
+})
 
 onBeforeMount(() => {
   axios
@@ -22,25 +47,20 @@ onBeforeMount(() => {
         dataAvailable.value = true;
         totalRows.value = places.value.length;
       })
-      .catch((error) => {
-        console.error(error);
-      });
+      .catch(console.error);
+
+  axios
+      .get('http://localhost:5000/places/types')
+      .then((response) => {
+        for (const typeData of response.data) {
+          types.value.push({
+            value: typeData[0],
+            text: `${typeData[0].replaceAll('_', ' ')} (${typeData[1]})`
+          })
+        }
+      })
+      .catch(console.error);
 });
-
-/*** Table properties ***/
-const fields = [
-  {key: 'id', sortable: true},
-  {key: 'n_photos', sortable: true},
-  {key: 'n_reviews', sortable: true},
-  {key: 'name', sortable: true},
-  {key: 'rating', sortable: true},
-  {key: 'is_market', sortable: true}
-];
-
-const perPage = 20;
-const currentPage = ref(1);
-const filter = ref(null);
-const totalRows = ref(0)
 
 
 const MAP_LINK_TEMPLATE = 'https://www.google.com/maps/place/?q=place_id:PLACE_ID';
@@ -88,6 +108,19 @@ function updatePlaceData(placeId, placeData, recordProcessed) {
   }
 }
 
+function doFilter(item, filterProp) {
+  let text = filterProp.text;
+  let type = filterProp.type;
+
+  let filterText = (text !== null && text !== '');
+  let filterType = (type !== null && type !== '');
+
+  let textMatch = filterText && item.name.includes(text);
+  let typeMatch = filterType && item.types.includes(type);
+
+  return (!filterText || textMatch) && (!filterType || typeMatch);
+}
+
 function onFiltered(filteredItems) {
   // Trigger pagination to update the number of buttons/pages due to filtering
   totalRows.value = filteredItems.length
@@ -113,15 +146,20 @@ function onFiltered(filteredItems) {
           <b-input-group size="sm">
             <b-form-input
                 id="filter-input"
-                v-model="filter"
+                v-model="textFilter"
                 type="search"
                 placeholder="Type to Search"
-            ></b-form-input>
-
+            />
             <b-input-group-append>
-              <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+              <b-button :disabled="!textFilter" @click="textFilter = ''">Clear</b-button>
             </b-input-group-append>
           </b-input-group>
+        </b-col>
+        <b-col lg="4">
+          <select v-model="typeFilter">
+            <option value="">Select an option</option>
+            <option v-for="type in types" :key="type.value" :value="type.value">{{ type.text }}</option>
+          </select>
         </b-col>
       </b-row>
       <b-table id="places-table"
@@ -137,7 +175,7 @@ function onFiltered(filteredItems) {
                :per-page=perPage
                :current-page=currentPage
                :filter="filter"
-               filter-included-fields="[name, types]"
+               :filter-function="doFilter"
                :tbody-tr-class="rowClass"
                @row-contextmenu="togglePlaceDetails"
                @filtered="onFiltered"
